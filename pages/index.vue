ffc14afab18d24c6c7be94355b71579d01f4fbf5
CJ <smile9711234@gmail.com>
2022-03-23 15:52:04 +0800

    Request read

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    {{ title }}
+    <div v-for="office in offices" :key="office.id">
+      {{ office.name }}
+    </div>
+
+    <button @click="fetch">
+      click
+    </button>
   </div>
 </template>
 
-<script setup>
-import { ref } from '@nuxtjs/composition-api'
+<script>
+export default {
+  data () {
+    return {
+      offices: []
+    }
+  },
 
-const title = ref('hello')
+  methods: {
+    async fetch () {
+      const response = await this.$axios.$post('/api/test')
+      this.offices = response
+    }
+  }
+}
 </script>
