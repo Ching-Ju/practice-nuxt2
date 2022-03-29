a19045272b0df22eb8a86b2aa2069bed95cf6167
CJ <smile9711234@gmail.com>
2022-03-29 22:43:10 +0800

    Access page Layout empty add head

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -4,6 +4,18 @@
 
 <script>
 export default {
-  name: 'Empty'
+  name: 'Empty',
+
+  head () {
+    return {
+      link: [
+        {
+          id: 'theme-link',
+          rel: 'stylesheet',
+          href: `/themes/${this.$store.state.appState.theme}/theme.css`
+        }
+      ]
+    }
+  }
 }
 </script>
