08fa1c8df8db03c39555f8c19a87edd8e466877d
CJ <smile9711234@gmail.com>
2022-04-01 08:36:11 +0800

    Draft crud

---
+++ components/crud/RestUserComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <card-style2-component card-title="REST">
+    <template #cardBody>
+      <DataTable :value="$store.state.product.products" responsive-layout="scroll">
+        <Column field="code" header="Code" export-header="Product Code" />
+        <Column field="name" header="Name" />
+        <Column field="category" header="Category" />
+        <Column field="quantity" header="Quantity" />
+      </DataTable>
+    </template>
+  </card-style2-component>
+</template>
+
+<script setup>
+</script>
---
+++ pages/crud.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="grid">
+    <div class="col-12">
+      <crud-rest-user-component />
+    </div>
+  </div>
+</template>
