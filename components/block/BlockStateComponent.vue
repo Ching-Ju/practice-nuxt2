ceac5ea89e2f7221e6883e31f5f44ea89da0c3f1
CJ <smile9711234@gmail.com>
2022-04-15 21:14:32 +0800

    Block page Pricing, Call to Action, Banner, Page Heading, Stats components

---
+++ components/block/BlockStateComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <CardStyle2Component card-title="Pricing">
+    <template #cardBody>
+      <div class="grid">
+        <component
+          :is="component"
+          v-for="(component, index) in components"
+          :key="index"
+          class="col-12 lg:col-6 xl:col-3 md:col-3"
+        />
+      </div>
+    </template>
+  </CardStyle2Component>
+</template>
+
+<script setup>
+const components = [
+  'DashboardOrderComponent',
+  'DashboardRevenueComponent',
+  'DashboardCustomerComponent',
+  'DashboardCustomerComponent'
+]
+</script>
--- pages/blocks.vue
+++ pages/blocks.vue
@@ -7,5 +7,25 @@
     <div class="col-12">
       <BlockFeatureComponent />
     </div>
+
+    <div class="col-12">
+      <BlockPricingComponent />
+    </div>
+
+    <div class="col-12">
+      <BlockCallToActionComponent />
+    </div>
+
+    <div class="col-12">
+      <BlockBannerComponent />
+    </div>
+
+    <div class="col-12">
+      <BlockPageHeadingComponent />
+    </div>
+
+    <div class="col-12">
+      <BlockStateComponent />
+    </div>
   </div>
 </template>
