96ed331c42d397cf0a94b142f5ce626e370d27fd
CJ <smile9711234@gmail.com>
2022-05-11 11:24:37 +0800

    Animate directive

--- components/landing2/Landing2CooperateComponent.vue
+++ components/landing2/Landing2CooperateComponent.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
+    <div class="col-12 text-center">
+      <h1 v-animate:animate__rubberBand="100">
+        Cooperates
+      </h1>
+    </div>
+
     <div class="flex justify-content-center animate__shakeX">
-      <img src="/images/cooperate.png" width="120px" class="animate__animated animate__flipInY">
-      <img src="/images/cooperate.png" width="120px">
-      <img src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
     </div>
     <div class="flex justify-content-center" style="transform: translateY(-50%)">
-      <img src="/images/cooperate.png" width="120px">
-      <img src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
     </div>
     <div class="flex justify-content-center" style="transform: translateY(-100%)">
-      <img src="/images/cooperate.png" width="120px">
+      <img v-animate:animate__flipInY="100" src="/images/cooperate.png" width="120px">
     </div>
   </div>
 </template>
---
+++ plugins/animate.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+Vue.directive('animate', function (el, binding) {
+  el.classList.add('animate__animated')
+  window.addEventListener('scroll', scroll)
+
+  function scroll () {
+    const clientHeight = document.documentElement.clientHeight
+    const elementHeight = el.getBoundingClientRect().top
+    const elementAnimate = binding.arg
+    const delay = binding.value
+    let elementTimeOut = null
+
+    if (!elementAnimate) {
+      return
+    }
+
+    if (elementHeight < clientHeight) {
+      if (delay) {
+        elementTimeOut = setTimeout(() => {
+          el.classList.add(elementAnimate)
+          el.style.opacity = 1
+        }, delay)
+      } else {
+        el.classList.add(elementAnimate)
+        el.style.opacity = 1
+      }
+    } else {
+      if (elementTimeOut) {
+        window.clearTimeout(elementTimeOut)
+      }
+      el.classList.remove(elementAnimate)
+      el.style.opacity = 0
+    }
+  }
+})
