e0324bb1d04277648f1cfee247e295f28c318927
CJ <smile9711234@gmail.com>
2022-06-04 15:45:19 +0800

    QR Code page

--- layouts/default.vue
+++ layouts/default.vue
@@ -59,7 +59,8 @@ export default {
             { label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc' },
             { label: 'Map', icon: 'pi pi-fw pi-map', to: '/map' },
             { label: 'Video', icon: 'pi pi-fw pi-video', to: '/video' },
-            { label: 'Credit Card', icon: 'pi pi-fw pi-credit-card', to: '/credit-card' }
+            { label: 'Credit Card', icon: 'pi pi-fw pi-credit-card', to: '/credit-card' },
+            { label: 'QR Code', icon: 'pi pi-fw pi-qrcode', to: '/qrcode' }
           ]
         },
         {
---
+++ pages/qrcode.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="grid">
+    <div class="col-6">
+      <BlockViewer header="qrcode.vue" :code="code">
+        <QrcodeVue :value="'https://www.google.com/'" :size="300" level="H" />
+      </BlockViewer>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import QrcodeVue from 'qrcode.vue'
+
+const code = `npm i qrcode.vue@1.7
+
+import QrcodeVue from 'qrcode.vue'
+
+<QrcodeVue :value="'https://www.google.com/'" :size="300" level="H" />`
+</script>
